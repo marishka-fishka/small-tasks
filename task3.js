faf3a4932d24793fa82b11e606e91e5d21a14f50
marishka-fishka <ya.m.l.12ym@gmail.com>
2021-11-22 21:33:13 -0500

    task-3

---
+++ task3.js
@@ -0,0 +1,6 @@
+//Math.ceil function always rounds a number up to the next largest whole number or integer
+
+function century(year) {
+    var century=year/100;
+    return Math.ceil(century);
+  }
