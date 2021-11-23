d72aeb9c0468a8ac6deafcf6a2cbc24b61945600
marishka-fishka <ya.m.l.12ym@gmail.com>
2021-11-23 18:14:57 -0500

    task-5

---
+++ task5.js
@@ -0,0 +1,6 @@
+
+function highAndLow(numbers){
+    // you are given a string of space separated numbers, and have to return the highest and lowest number.
+    numbers = numbers.split(" ");
+    return Math.max.apply(null, numbers)+ " "+ Math.min.apply(null, numbers)
+  }
