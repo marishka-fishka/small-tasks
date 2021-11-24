9eb83b36cc68178174b5e75aec67b10fa825fd1b
marishka-fishka <ya.m.l.12ym@gmail.com>
2021-11-23 22:13:29 -0500

    task6-7

--- task6.js
+++ task6.js
@@ -1,3 +1,9 @@
-function sumTwoSmallestNumbers(numbers) {  
-    //
+// a function that returns the sum of the two lowest positive numbers given an array  
+    function sumTwoSmallestNumbers(numbers) {  
+        //Code here
+        var result=numbers.sort((a,b)=>a-b).slice(0,2);
+        var sum=result[0]+result[1]
+        return sum;
 }
+        
+
---
+++ task7.js
@@ -0,0 +1,8 @@
+// double value of existing array
+function maps(x){
+    var arr = []; 
+    for (var i=0; i<x.length; i++){
+        arr.push(x[i]*2);
+    }
+    return arr;
+    }
