82a5b0415ce282f602b2cca23ef93cd048d69fc6
marishka-fishka <ya.m.l.12ym@gmail.com>
2021-11-26 23:04:10 -0500

    task18

---
+++ task18.js
@@ -0,0 +1,9 @@
+//Write a function that when given a URL as a string, parses out just the domain name and returns it as a string f.e. github
+
+function domainName(url){
+
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+    return url.split('.')[0];
+};
